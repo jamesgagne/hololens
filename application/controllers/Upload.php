492225f6f22eaa724f1aa1b51e331568d532da6f
Webber <000352627@mohawkcollege.ca>
2018-01-04 12:47:32 -0500

    Fixing Stuff That Was Broken... By James W

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -23,6 +23,8 @@ class Upload extends CI_Controller {
 		else
 		{
 			$this->TPL['uploadsuccess'] = false;
+      $this->TPL['colors'] = $this->UploadModel->getColors();
+      $this->TPL['categories'] = $this->UploadModel->getCategories();
 			$this->template->show('upload', $this->TPL);
 		}	
 	}
@@ -66,7 +68,7 @@ class Upload extends CI_Controller {
                           
                         }*/
 
-      /*$src = $_FILES['fbx']['tmp_name'];
+      $src = $_FILES['fbx']['tmp_name'];
       $destination  = hardImgUrl(). $_FILES['fbx']['name'];
 
       move_uploaded_file($src,$destination);
@@ -78,8 +80,7 @@ class Upload extends CI_Controller {
       $color_id = 1;
       $this->UploadModel->insertFile($name, $description, $savePath,$size,$category_id, $color_id); 	
       $this->TPL['uploadsuccess'] = true;
-      $this->template->show('upload', $this->TPL);	*/
-      echo json_encode("Post= " . print_r($_POST) . "Files= ". print_r($_FILES) );		
+      $this->template->show('upload', $this->TPL);		
   }
     
   function extension($path) { 
@@ -91,4 +92,60 @@ class Upload extends CI_Controller {
 
   return $extension; 
   } 
+  public function addnew(){
+      $src = $_FILES['model']['tmp_name'];
+      $destination  = hardImgUrl(). $_FILES['model']['name'];
+
+      move_uploaded_file($src,$destination);
+      $savePath = assetUrl() . "models/".$_FILES['model']['name'];
+      $name = $_FILES['model']['name'];
+      $description = $_POST['description'];
+      
+      $category_id = $_POST['category'];
+      $color_id = $_POST['color'];
+      $thisID = $this->UploadModel->insertModel($name, $description, $savePath, $category_id, $color_id);   
+      $this->TPL['success'] = true;
+      $this->TPL['newModelID'] = $thisID;
+      //$this->template->show('upload', $this->TPL);    
+  
+
+    /*$org = $this->getOrganization();
+    $_POST['validated'] = $this->validatePost();  
+                  
+    if($_POST['validated']){
+
+        $resp = $this->addToDB();
+        if ($resp['sucess']){
+          $src = $_FILES['file']['tmp_name'];
+          $destination  ="/home/student/000328298/public_html/private/CloudPOS/application/assets/img/" . $org['name'] ."/". $_FILES['file']['name'];
+          copy($src, $destination);
+        }
+        $resp['validated'] = true;
+        echo json_encode($resp);    
+     }
+    else{
+      $resp = array();
+        $resp['validated'] = false;
+        $resp['sucess'] = false;
+        $resp['errors'] = $this->postErrors;
+        echo json_encode($resp);
+      }       */
+      echo json_encode($this->TPL);       
+    
+  }
+  public function addThumb(){
+    $src = $_FILES['thumb']['tmp_name'];
+      $destination  = hardImgUrl()."/thumbs/". $_FILES['thumb']['name'];
+
+      move_uploaded_file($src,$destination);
+      $savePath = assetUrl() . "models/thumbs/".$_FILES['thumb']['name'];
+      $description = $_FILES['thumb']['name'];
+      $model_id = $_POST['model_id'];
+      $picture_id = $this->UploadModel->insertImage($description, $savePath);   
+      $this->TPL['newPictureID'] = $picture_id;
+      $this->TPL['updated'] = $this->UploadModel->updateModelsPicture($model_id, $picture_id);
+      echo json_encode($this->TPL);
+  }
+
 }
+
--- application/views/main/upload.php
+++ application/views/main/upload.php
@@ -18,31 +18,137 @@
   text-align: center;
   margin-top: 5%
 }
+fieldset{
+  border: 1px dashed black;
+}
 </style>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script>
-  var fileCount = 0;
-function drag_drop(event) {
-    event.preventDefault();
-    jQuery.each(event.dataTransfer.files,function(element){ 
-      $("#files").append("<fieldset id='"+fileCount+"'> <h2>"+event.dataTransfer.files[element].name+"</h2>Add a Description &nbsp;<input type='text' name='"+fileCount+"["+event.dataTransfer.files[element].name+"]description' /> <br />Select a thumbnail &nbsp;<input required type='file' name='"+fileCount+"["+event.dataTransfer.files[element].name+"]thumbFile' /></br></fieldset");
-      fileCount ++;
-    });    
-}
-function changedFileUpload(e){
-  var control = document.getElementById("your-files");
-   jQuery.each(control.files,function(element){ 
-      $("#files").append("<fieldset id='"+fileCount+"'> <h2>"+control.files[element].name+"</h2>Add a Description &nbsp;<input type='text' name='"+fileCount+"["+control.files[element].name+"]description' /> <br />Select a thumbnail &nbsp;<input required type='file' name='"+fileCount+"["+control.files[element].name+"]thumbFile' /></br></fieldset");
-      fileCount++;
-    });
-}
-</script>
+
 <div id="drop_zone" ondrop="drag_drop(event)" ondragover="return false"><div id="makeSelection">Drag And Drop or<br /><input style="border: 2px dashed #999;" type='file' multiple="true" onchange="changedFileUpload(event)" id="your-files" /></div></div>
-<form id="files" enctype ='multipart/form-data' action="<?=base_url()?>index.php/Upload/newFile" method="post">
+<form id="files" enctype ='multipart/form-data' method="post">
   <br />
-  <button type="submit">Upload</button>
+  <button id="formButton" type="submit">Upload</button>
   <br />
     </div>
   </div>
-  
+  <script>
+  var fileCount = 0;
+  var uploadedModels = [];
+function drag_drop(event) {
+    event.preventDefault();
+    jQuery.each(event.dataTransfer.files,function(element){
+        if (getType(event.dataTransfer.files[element].name)=="fbx"){
+
+    var colorSelect = "Select a Color: <select class='select' name='colors' id='"+fileCount+"color' style='width:10%;'><option value='' disabled selected>Select</option>";
+                
+                <?php foreach ($colors as $key => $value) :?>
+                colorSelect+="<option value='<?=$value['color_id']?>'> <?=$value['name']?></option>";
+                <?php endforeach?>
+    colorSelect+="</select><br />";
+
+        var catSelect = "Select a Category: <select class='select' name='colors' id='"+fileCount+"category' style='width:10%;'><option value='' disabled selected>Select</option>";
+                
+                <?php foreach ($categories as $key => $value) :?>
+                catSelect+="<option value='<?=$value['category_id']?>'> <?=$value['name']?></option>";
+                <?php endforeach?>
+        catSelect+="</select><br />";
+
+      $("#files").append("<fieldset id='"+fileCount+"'> <h2>"+event.dataTransfer.files[element].name+"</h2>Add a Description &nbsp;<input type='text' name='"+event.dataTransfer.files[element].name+"[description]' id='"+fileCount+"description' /> <br /><br />Select a thumbnail &nbsp;<input required type='file' name='"+event.dataTransfer.files[element].name+"[thumbFile]' id='"+fileCount+"thumbFile' /><br />"+colorSelect+"<br />"+catSelect+"<br /></fieldset>");
+      uploadedModels.push(event.dataTransfer.files[element]);
+      console.log(event.dataTransfer.files[element].type);
+      fileCount ++;
+    }
+    else{
+      alert(event.dataTransfer.files[element].name+" is not of type fbx");
+    }
+    });
+    console.log(uploadedModels);    
+}
+function changedFileUpload(e){
+  var control = document.getElementById("your-files");
+   jQuery.each(control.files,function(element){
+    
+    if (getType(control.files[element].name)=="fbx"){
+   var colorSelect = "Select a Color: <select class='select' name='colors' id='"+fileCount+"color' style='width:10%;'><option value='' disabled selected>Select</option>"; 
+
+   <?php foreach ($colors as $key => $value) :?>
+                colorSelect+="<option value='<?=$value['color_id']?>'> <?=$value['name']?></option>";
+        <?php endforeach?>
+        colorSelect+="</select><br />";
+  var catSelect = "Select a Category: <select class='select' name='colors' id='"+fileCount+"category' style='width:10%;'><option value='' disabled selected>Select</option>";
+                
+                <?php foreach ($categories as $key => $value) :?>
+                catSelect+="<option value='<?=$value['category_id']?>'> <?=$value['name']?></option>";
+                <?php endforeach?>
+        catSelect+="</select><br />";
+      $("#files").append("<fieldset id='"+fileCount+"'> <h2>"+control.files[element].name+"</h2>Add a Description &nbsp;<input type='text' name='"+control.files[element].name+"[description]' id='"+fileCount+"description' /> <br /><br />Select a thumbnail &nbsp;<input required type='file' name='"+control.files[element].name+"[thumbFile]' id='"+fileCount+"thumbFile' /><br />"+colorSelect+"</br>"+catSelect+"<br /></fieldset>");
+      uploadedModels.push(control.files[element]);
+      console.log(control.files[element].type);
+      fileCount++;
+    }
+    else{
+      alert(control.files[element].name+" is not type fbx");
+    }
+    });
+   console.log(uploadedModels);
+}
+
+$("#formButton").click(function(event){
+    event.preventDefault();
+   jQuery.each(uploadedModels,function(element){
+    var form_data = new FormData();
+    form_data.append("model",uploadedModels[element]);
+console.log($('#'+element+'description').val());
+    //console.log($('#'+uploadedModels[element].name+'[description]').val());
+    form_data.append("description",$('#'+element+'description').val());
+    form_data.append("color",$('#'+element+'color').val());
+    form_data.append("category",$('#'+element+'category').val());
+    $.ajax({
+                url: 'Upload/addnew', // point to server-side PHP script 
+                dataType: 'text',  // what to expect back from the PHP script, if anything
+                cache: false,
+                contentType: false,
+                processData: false,
+                data: form_data,                         
+                type: 'post',
+                success: function (data)
+                {
+                  console.log(data); 
+                  var arr = JSON.parse(data);
+                  if ('newModelID' in arr){
+                    var newID = arr.newModelID;
+                    var form_data = new FormData();
+                    file_data = $('#'+element+"thumbFile").prop('files')[0];
+                    form_data.append('model_id', newID);
+                    form_data.append("thumb",file_data);
+                    $.ajax({
+                        url: 'Upload/addThumb', // point to server-side PHP script 
+                        dataType: 'text',  // what to expect back from the PHP script, if anything
+                        cache: false,
+                        contentType: false,
+                        processData: false,
+                        data: form_data,                         
+                        type: 'post',
+                        success: function (data)
+                        {
+                            console.log(data);
+                        }
+                      });
+
+                  }
+                  else{
+                    alert("An error was encounter uploading file: "+uploadedModels[element].name+" please try this one again");
+                  }
+                }
+
+    });
+   });
+   //location.reload();
+       
+});
+function getType(str) {
+    return str.split('.')[1];
+}
+
+</script>
 
